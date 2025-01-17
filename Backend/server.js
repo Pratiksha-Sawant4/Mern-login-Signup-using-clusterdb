ecb1e19971bf8ab4c977b722812abda6f83c1721
Pratiksha-Sawant4 <sawantpratiksha477@gmail.com>
2025-01-17 16:43:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,13 @@ const authRoutes = require('./routes/auth');
 dotenv.config();
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+module.exports = app;
+
 app.use(express.json());
 app.use(cors(
      {
