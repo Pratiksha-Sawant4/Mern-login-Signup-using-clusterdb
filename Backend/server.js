8f191e776bcbed5e208b41ae5cbecabe752307fa
Pratiksha-Sawant4 <sawantpratiksha477@gmail.com>
2025-01-17 15:48:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+     {
+    origin : [],
+    methods : ["POST", "GET"],
+    credentials : true
+  }
+));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
